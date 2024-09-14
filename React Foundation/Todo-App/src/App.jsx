9e4f4e2efe261f9dc726ff3e79d6a3ad0069edcb
ZEESHAN HASAN KHAN <zeeshan54khan@gmail.com>
2024-09-14 23:39:12 +0530

    Mongo DB and React

--- React Foundation/Todo-App/src/App.jsx
+++ React Foundation/Todo-App/src/App.jsx
@@ -60,23 +60,119 @@
 // }
 
 // export default App
-import { useState } from 'react'
 
-import './App.css'
+//Example 1
+// import { useState } from 'react'
+// import React from 'react'
+
+// //
+// //React.memo() skip re-rendering when the state of the component doen't change
+// const Header = React.memo(function Header({ title }) {
+//   return <>
+//     <h1>
+//       {title}
+//     </h1>
+//   </>
+// })
+
+// function App() {
+//   function submit() {
+//     setName(Math.random());
+//   }
+//   const [name, setName] = useState('Z');
+//   return (
+//     <div>
+//       <button onClick={submit}>Click me to change the title</button>
+//       <Header title={name}></Header>
+//       <Header title="X"></Header>
+//       <Header title="y"></Header>
+//       <Header title="a"></Header>
+//     </div>
+//   )
+// }
+
+// export default App
 
 
-function App() {
-  function submit() {
-    setName(Math.random());
-  }
-  const [name, setName] = useState('Zeeshan');
-  return (
-    <>
-      <button onClick={submit}>Click me to change the title</button>
-      <h1>My Name is {name}</h1>
-      <h1>My Name is Hasan</h1>
-    </>
-  )
+//Example 2: Key For list array
+// import { useState } from 'react'
+// import React from 'react'
+
+// function Todo({ title, description }) {
+//   return (
+//     <>
+//       <h1>{title}</h1>
+//       <h2>{description}</h2>
+//     </>
+//   )
+// }
+// let counter = 3;
+// function App() {
+//   const [todo, setTodo] = useState([{
+//     id: 1,
+//     title: "Go to Gym",
+//     description: "Go to gym Today"
+//   },
+//   {
+//     id: 2,
+//     title: "Eat Food",
+//     description: "Eat food"
+//   }])
+
+
+
+//   function addTodo() {
+//     setTodo([...todo, {
+//       id: counter++,
+//       title: Math.random(),
+//       description: Math.random()
+//     }])
+//   }
+//   return (
+//     <div>
+//       <button onClick={addTodo}>Add Todo</button>
+//       {todo.map((todo) => {
+//         return <Todo key={todo.id} title={todo.title} description={todo.description}></Todo>
+//       })}
+//     </div>
+//   )
+// }
+
+// export default App
+
+import { useEffect, useState } from 'react'
+import React from 'react'
+
+function Todo(props) {
+  return <>
+    <h1>{props.title}</h1>
+    <h1>{props.description}</h1>
+  </>
 }
+let counter = 1;
+function App() {
+  const [todos, setTodos] = useState([{
+    id: counter,
+    title: 'Z',
+    description: 'H'
+  }]);
 
-export default App
+  function addTodo() {
+    setTodos([...todos, { id: ++counter, title: Math.random(), description: Math.random() }])
+  }
+
+  useEffect(() => {
+    console.log('First Time Render It will show');
+  }, [])
+  return (
+    <div>
+      <button onClick={addTodo}>Add Todo</button>
+      {todos.map((todo) => {
+        return <Todo key={todo.id} title={todo.title} description={todo.description} />
+      })}
+    </div>
+  )
+
+
+}
+export default App;
